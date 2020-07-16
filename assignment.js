368e1ac59a80a2cf7a68a66c03f4e1d9d009a3f0
Zahidulhussen <mujahidbulbul01@gmail.com>
2020-07-17 04:02:00 +0600

    woodCalculator done

--- assignment.js
+++ assignment.js
@@ -1,4 +1,5 @@
 //feetToMile
+
 function feetToMile(feet){
     var mile = feet / 5280;
     return mile;
@@ -9,3 +10,14 @@ var brotherMile = feetToMile(47520);
 console.log(brotherMile);
 var uncleMile = feetToMile(79200);
 console.log(uncleMile);
+
+// woodCalculator
+function woodCalculator(chair,table,bed){
+    var chairCount = 5 * 1;
+    var tableCount = 5 * 3;
+    var bedCount = 5 * 5;
+    var totalWood = chairCount + tableCount + bedCount;
+    return totalWood;
+}
+var woodResult = woodCalculator(1 , 3, 5);
+console.log(woodResult);
